56cb06b1ae6c74ec77c341cf1b60fda5d743a714
LenaGolovach11 <kemeev.maks@gmail.com>
2026-05-25 07:52:31 +0300

    2Comm

--- while_con_br.py
+++ while_con_br.py
@@ -2,12 +2,17 @@
 # Затем:
 # если в числе есть цифры 1 или 5, то программа считает их количество;
 # если в числе есть цифра 7, то программа останавливается и выводит текст "В программе есть запрещенная цифра"
+# выводится сумма всех цифр и их количество
 
-flag = ""
+flag = True
 count5 = 0
 count1 = 0
+sum_num = 0
+count = 0
+max_num = 0
+min_num = 0
 
-while flag != "end":
+while flag == True:
 
     print("Введите число")
     print()
@@ -29,10 +34,17 @@ while flag != "end":
             print("В программе есть запрещенная цифра")
             print()
             break
+        if 
+
+        sum_num += last
+        count += 1
 
         num //= 10
     else:
         print()
-        print(f"Количество цифр 1 - {count1}")
-        print(f"Количество цифр 5 - {count5}")
+        print(f"Количество цифр - {count}")
+        print(f"Сумма цифр - {sum_num}")
+        print()
+        print(f'Количество цифр "1" - {count1}')
+        print(f'Количество цифр "5" - {count5}')
         print()
